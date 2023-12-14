5db76b579c1e8d767ea4ec57e3eca85b3f3baf13
viniciusDantas <72734130+vdc50@users.noreply.github.com>
2023-12-14 23:53:44 +0000

    add `compose` config file with database service

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,4 @@
+// Utilizando TDD no primeiro teste de integração em server externo
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
