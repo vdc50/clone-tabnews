34fef1b5f0f19d7145e3a72e83ea3cf77a48859e
viniciusDantas <72734130+vdc50@users.noreply.github.com>
2023-12-24 21:58:34 +0000

    add `serivces` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 function Home() {
-  return <div> index.js </div>;
+  return (
+    <div>
+      {"Proposta de Opção de Compra"}
+      <p></p>
+      {"Ativo indicado: Sala comercial na Av: Champanhart..."}
+      <p></p>
+      {
+        "A proposta cabe a condição de aluguel inicial que será subtraida do valor total da venda."
+      }
+      <p></p>
+      {
+        "Dentro deste periódo o valor de sinal de 50 mil será usado para acumulação total do bem."
+      }
+      <p></p>
+      {
+        "Os valores serão apresentados neste canal para eveitos de apresentar a evolução da receita obtida."
+      }
+      <p></p>
+    </div>
+  );
 }
 
 export default Home;
